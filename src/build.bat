c5fae69841ffa5e1da73043b70362fc982f32137
uhwang <43251090+uhwang@users.noreply.github.com>
2021-02-13 07:33:28 -0600

    Copy default.docx

--- src/build.bat
+++ src/build.bat
@@ -1,5 +1,13 @@
+del .\XRef\Xref.exe
+del .\XRef\library.zip
+
 python setup.py build
+
 copy /y kbible_list.txt .\XRef
 copy /y kbible_path.txt .\XRef
 copy /y ebible_list.txt .\XRef
 copy /y ebible_path.txt .\XRef
+
+copy /y default.docx .\XRef
+copy /y default-settings.xml .\XRef
+
